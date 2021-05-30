6b9afcd8c77440eb4b60be8e62643ce8e4804441
huyurun <1409520200@qq.com>
2021-05-30 14:33:59 +0800

    :art: add two numbers add

---
+++ leetcode1.js
@@ -0,0 +1,22 @@
+// 两数相加
+
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/two-sum
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// 利用哈希表
+
+const twoNums = function(nums, target) {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.get(nums[i] - target)) {
+      return [map.get(nums[i] - target), i]
+    } else {
+      map.set(nums[i], i)
+    }
+  }
+}
