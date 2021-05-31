1ca86e2d551c8d622ea1a005d6ea7b1eec485927
huyurun <1409520200@qq.com>
2021-05-31 23:14:17 +0800

    :sparkles: leetcode 9

---
+++ leetcode9.js
@@ -0,0 +1,13 @@
+// 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+// 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+
+const isPalidrome = function (x) {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false
+  }
+  let reverseNumber = 0
+  while (x > reverseNumber) {
+    reverseNumber = x % 10 + reverseNumber * 10
+  }
+  return x === reverseNumber || x === Math.floor(reverseNumber / 10)
+}
